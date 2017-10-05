1d496235c8a66b4ec6dc13885ce25a863f6fe56b
Daniel Nautré <dnautre@steelcase.com>
2017-10-05 15:21:42 +0200

    Add some more gear to buy, rebalance the rest

--- src/gear.py
+++ src/gear.py
@@ -3,12 +3,15 @@
 
 
 gear = {
-    'Stick': {'type': 'sword', 'lvl': 1, 'price': 0, 'damage': 0.2, 'speed': 1.2, 'tt': 'A small and fragile stick found on the ground'},
-    'Sandals': {'type': 'feet', 'lvl': 1, 'price': 5, 'armor': 1, 'tt': 'Overpriced plastics thongs. what\'s plastic anyway ?'},
+    'Stick': {'type': 'weapon', 'subtype': 'mace', 'lvl': 1, 'price': 0, 'damage': 0.3, 'speed': 1.4, 'tt': 'A small and fragile stick found on the ground'},
+    'Sandals': {'type': 'feet', 'lvl': 1, 'price': 5, 'armor': 0.5, 'tt': 'Overpriced plastics thongs. what\'s plastic anyway ?'},
     'Gomina': {'type': 'head', 'lvl': 1, 'price': 15, 'armor': 0.5, 'tt': 'From a distance, it almost looks like you\re wearing an helmet'},
-    'Bandana': {'type': 'head', 'lvl': 1, 'price': 25, 'armor': 1, 'tt': 'Fashionnable piece of cloth with a pearsley pattern printed on it, thin but good looking'},
     'Potato sac': {'type': 'torso', 'lvl': 1, 'price': 20, 'armor': 1, 'tt': 'Better than nothing I suppose.'},
-    'Dragonforce T-shirt': {'type': 'torso', 'lvl': 1, 'price': 45, 'armor': 1.5, 'tt': 'Nothing is more medieval than a Dragonforce T-shirt.'}
+    'Bandana': {'type': 'head', 'lvl': 1, 'price': 55, 'armor': 1, 'tt': 'Fashionnable piece of cloth with a pearsley pattern printed on it, thin but good looking'},
+    'Dragonforce T-shirt': {'type': 'torso', 'lvl': 1, 'price': 65, 'armor': 1.5, 'tt': 'Nothing is more medieval than a Dragonforce T-shirt.'},
+    'Club': {'type': 'weapon', 'subtype': 'mace', 'lvl': 1, 'price': 70, 'damage': 1, 'speed': 1.1, 'tt': 'A solid piece of oak wood'},
+    'Worn Shoes': {'type': 'feet', 'lvl': 1, 'price': 95, 'armor': 1.4, 'tt': 'They smell like bad cheese but they provide some protection'},
+    'Dagger': {'type': 'weapon', 'subtype': 'sword', 'lvl': 2, 'price': 250, 'damage': 1.2, 'speed': 1.5, 'tt': 'A quick and deadly weapon, if you can get close enough'}
 }
 
 
@@ -32,4 +35,3 @@ def get_available_gear(lvl):
             available[name] = stuff
 
     return available
-
