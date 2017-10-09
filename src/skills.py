ffcc83c9ed31949e1dad48fc22961d7027d61abe
Daniel Nautré <dnautre@steelcase.com>
2017-10-09 10:34:55 +0200

    Add new skills

--- src/skills.py
+++ src/skills.py
@@ -2,9 +2,11 @@
 # -*- coding: utf8 -*
 
 skills = {
-    'Strength': {'lvl': 1, 'desc': 'Damage is increased by <strong>{value}</strong>%', 'tt': 'The stronger you become, the harder you hit'},
-    'Dexterity': {'lvl': 1, 'desc': 'Armor is increased by <strong>{value}</strong>%', 'tt': 'Avoid enemy blows'},
-    'Intelligence': {'lvl': 1, 'desc': 'you gain <strong>{value}</strong>% more XP per second', 'tt': 'Wisdom helps you learn faster'}
+    'Strength': {'lvl': 1, 'lvl_step': 1, 'step': 2, 'desc': 'Damage is increased by <strong>{value}</strong>%', 'tt': 'The stronger you become, the harder you hit'},
+    'Dexterity': {'lvl': 1, 'lvl_step': 1, 'step': 2, 'desc': 'Armor is increased by <strong>{value}</strong>%', 'tt': 'Avoid enemy blows'},
+    'Intelligence': {'lvl': 1, 'lvl_step': 1, 'step': 2, 'desc': 'you gain <strong>{value}</strong>% more XP per second', 'tt': 'Wisdom helps you learn faster'},
+    'Fiery Weapon': {'lvl': 5, 'lvl_step': 5, 'step': 5, 'desc': 'You cause <strong>{value}</strong>% fire damage', 'tt': 'Your imbue your weapon with fire'},
+    'Mana Shield': {'lvl': 6, 'lvl_step': 3, 'step': 4, 'desc': 'Your armor value is increased by <strong>{value}</strong>%', 'tt': 'You focus your energy to create a protective mana shield'}
 
 }
 
